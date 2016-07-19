5018ae58344c3519745b1880aa49984cc68649f7
java <java>
2016-07-19 16:52:25 +0900

    10ea Hell

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -4,7 +4,9 @@ public class MainApplication {
 
 	public static void main(String[] args){
 		System.out.println("###################");
+		for(int i=0; i<10; i++){
 		System.out.println("Hello SecondGradle");
+		}
 		System.out.println("###################");
 
 	}
