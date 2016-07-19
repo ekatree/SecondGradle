a347ce3a5508efd11f9527ef3f0edc0d7e848865
java <java>
2016-07-19 16:50:06 +0900

    add ##########

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -3,7 +3,9 @@ package com.hybrid;
 public class MainApplication {
 
 	public static void main(String[] args){
+		System.out.println("###################");
 		System.out.println("Hello SecondGradle");
+		System.out.println("###################");
 
 	}
 
